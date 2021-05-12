50f34b06ca18abd365ac07ed206a81c0f90f5368
panas224 <panas.oleh2019@gmail.com>
2021-05-12 20:15:53 +0300

    Car app Main Page

---
+++ src/TODO/TodoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default  function TodoList(){
+    return(
+        <ul>
+            <a href={"https://2048game.com/ru/"}>1</a>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    )
+}
